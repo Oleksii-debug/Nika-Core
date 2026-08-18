a56ad9a523869887c400cfc140605fd732223b8d
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-18 20:58:47 +0300

    M8: export controlled experiment API

---
+++ src/nika_core/experiments/__init__.py
@@ -0,0 +1,28 @@
+from nika_core.experiments.contracts import (
+    ArtifactKind,
+    ExperimentDefinition,
+    ExperimentSnapshot,
+    ExperimentStatus,
+    MetricObservation,
+    MetricRule,
+    PromotionPolicy,
+    ReplayCase,
+    StrategyRef,
+)
+from nika_core.experiments.engine import ExperimentEngine
+from nika_core.experiments.repository import ExperimentRepository, InMemoryExperimentRepository
+
+__all__ = [
+    "ArtifactKind",
+    "ExperimentDefinition",
+    "ExperimentEngine",
+    "ExperimentRepository",
+    "ExperimentSnapshot",
+    "ExperimentStatus",
+    "InMemoryExperimentRepository",
+    "MetricObservation",
+    "MetricRule",
+    "PromotionPolicy",
+    "ReplayCase",
+    "StrategyRef",
+]
