f54b26e433e20e74154d0a8d204566d3ba43ccaf
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-19 17:14:09 +0300

    Export research document and artifact contracts

--- src/nika_core/research/__init__.py
+++ src/nika_core/research/__init__.py
@@ -1,3 +1,13 @@
+from nika_core.research.blobs import BlobStoreError, ContentAddressedBlobStore
+from nika_core.research.chunking import ChunkPolicy, chunk_text
+from nika_core.research.documents import (
+    DocumentIngestionError,
+    DocumentLimits,
+    DocumentSecurityError,
+    DocumentTooLargeError,
+    extract_document_file,
+    preflight_office_archive,
+)
 from nika_core.research.local import (
     LocalFileTooLargeError,
     LocalIngestionError,
@@ -6,8 +16,14 @@ from nika_core.research.local import (
     extract_local_file,
 )
 from nika_core.research.models import (
+    ArtifactIngestResult,
+    BlobArtifact,
     CorpusDocument,
     ExtractedDocument,
+    ExtractionRecord,
+    ExtractionStatus,
+    FolderIngestFailure,
+    FolderIngestResult,
     IngestDisposition,
     IngestResult,
     ResearchWorkspace,
@@ -20,8 +36,21 @@ from nika_core.research.repository import ResearchRepository
 from nika_core.research.service import LocalCorpusService
 
 __all__ = [
+    "ArtifactIngestResult",
+    "BlobArtifact",
+    "BlobStoreError",
+    "ChunkPolicy",
+    "ContentAddressedBlobStore",
     "CorpusDocument",
+    "DocumentIngestionError",
+    "DocumentLimits",
+    "DocumentSecurityError",
+    "DocumentTooLargeError",
     "ExtractedDocument",
+    "ExtractionRecord",
+    "ExtractionStatus",
+    "FolderIngestFailure",
+    "FolderIngestResult",
     "IngestDisposition",
     "IngestResult",
     "LocalCorpusService",
@@ -34,6 +63,9 @@ __all__ = [
     "SourceKind",
     "SourceSpec",
     "UnsupportedLocalFormatError",
+    "chunk_text",
+    "extract_document_file",
     "extract_local_file",
     "normalize_text",
+    "preflight_office_archive",
 ]
