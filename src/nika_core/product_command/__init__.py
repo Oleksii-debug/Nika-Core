61e506b3212930f44e3660f4d084e95f9b165e88
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-20 21:59:39 +0300

    PF5: expose integrated PF adapters

--- src/nika_core/product_command/__init__.py
+++ src/nika_core/product_command/__init__.py
@@ -8,14 +8,30 @@ from nika_core.product_command.contracts import (
     ProductStatusKind,
     ProductUserDecision,
 )
+from nika_core.product_command.coordinator_adapter import coordinator_status_entries
+from nika_core.product_command.deployment_adapter import (
+    deployment_status_entries,
+    execution_status_entries,
+)
+from nika_core.product_command.product_project_adapter import (
+    ProductProjectCommandService,
+    ProductProjectDecisionUnavailableError,
+    project_detail,
+)
 
 __all__ = [
     "CommandRouteDecision",
     "CommandRouteKind",
     "EvidenceReference",
+    "ProductProjectCommandService",
+    "ProductProjectDecisionUnavailableError",
     "ProductProjectDetail",
     "ProductProjectSummary",
     "ProductStatusEntry",
     "ProductStatusKind",
     "ProductUserDecision",
+    "coordinator_status_entries",
+    "deployment_status_entries",
+    "execution_status_entries",
+    "project_detail",
 ]
