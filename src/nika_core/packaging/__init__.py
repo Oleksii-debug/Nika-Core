836f865e175dead512c6f40c31e3e5527c3d3a9f
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-21 14:26:02 +0300

    Export distributable release evidence primitives

--- src/nika_core/packaging/__init__.py
+++ src/nika_core/packaging/__init__.py
@@ -1,20 +1,28 @@
 """Windows release packaging primitives."""
 
 from .release import (
+    DistributableEvidence,
     ReleaseFile,
     ReleaseManifest,
+    build_distributable_evidence,
     build_release_manifest,
+    verify_distributable_evidence,
     verify_release_manifest,
+    write_distributable_evidence,
     write_release_manifest,
 )
 from .windows import WindowsBuildPlan, default_windows_plan
 
 __all__ = [
+    "DistributableEvidence",
     "ReleaseFile",
     "ReleaseManifest",
     "WindowsBuildPlan",
+    "build_distributable_evidence",
     "build_release_manifest",
     "default_windows_plan",
+    "verify_distributable_evidence",
     "verify_release_manifest",
+    "write_distributable_evidence",
     "write_release_manifest",
 ]
