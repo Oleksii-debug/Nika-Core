e931c6e65176c633d43ceb2281717acbeea21004
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-23 17:37:23 +0300

    DEV29: export release recovery API

--- src/nika_core/packaging/__init__.py
+++ src/nika_core/packaging/__init__.py
@@ -1,5 +1,14 @@
 """Windows release packaging primitives."""
 
+from .recovery import (
+    DatabaseRecoveryError,
+    DatabaseRestoreResult,
+    DatabaseSnapshotManifest,
+    create_database_snapshot,
+    restore_database_snapshot,
+    verify_database_file_against_snapshot,
+    verify_database_snapshot,
+)
 from .release import (
     ReleaseFile,
     ReleaseManifest,
@@ -10,11 +19,18 @@ from .release import (
 from .windows import WindowsBuildPlan, default_windows_plan
 
 __all__ = [
+    "DatabaseRecoveryError",
+    "DatabaseRestoreResult",
+    "DatabaseSnapshotManifest",
     "ReleaseFile",
     "ReleaseManifest",
     "WindowsBuildPlan",
     "build_release_manifest",
+    "create_database_snapshot",
     "default_windows_plan",
+    "restore_database_snapshot",
+    "verify_database_file_against_snapshot",
+    "verify_database_snapshot",
     "verify_release_manifest",
     "write_release_manifest",
 ]
