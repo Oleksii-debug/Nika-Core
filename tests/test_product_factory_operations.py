03dd700ed61f1e25b5711036a32291757a86968b
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-24 12:37:02 +0300

    PF8: fix effect journal fake import order

--- tests/test_product_factory_operations.py
+++ tests/test_product_factory_operations.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from datetime import UTC, datetime
 
 import pytest
+from pf8_effect_journal_fake import MemoryEffectJournal
 
 from nika_core.product_factory_operations import (
     MaintenanceState,
@@ -19,7 +20,6 @@ from nika_core.product_factory_operations_contracts import (
     ServiceObservation,
     ServiceReplica,
 )
-from pf8_effect_journal_fake import MemoryEffectJournal
 
 
 def _sha(value: int) -> str:
