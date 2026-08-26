12885e3ee15b8a68499c69aea2e4ca5fc5e3b273
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-26 23:48:12 +0300

    SLOT6: add Product Search package exports

---
+++ src/nika_core/product_search/__init__.py
@@ -0,0 +1,25 @@
+"""Deterministic Product Search domain projection over Universal Research."""
+
+from nika_core.product_search.service import (
+    ProductAvailability,
+    ProductCard,
+    ProductObservation,
+    ProductSearchCodec,
+    ProductSearchCriteria,
+    ProductSearchError,
+    ProductSearchResult,
+    ProductSearchService,
+    ProductSort,
+)
+
+__all__ = [
+    "ProductAvailability",
+    "ProductCard",
+    "ProductObservation",
+    "ProductSearchCodec",
+    "ProductSearchCriteria",
+    "ProductSearchError",
+    "ProductSearchResult",
+    "ProductSearchService",
+    "ProductSort",
+]
