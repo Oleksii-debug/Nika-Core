a3ba2171837c2260e13aa2653b32e3b0fcb515f1
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-26 23:46:26 +0300

    MODEL-LAB-01: add Model Engineering Lab package surface

---
+++ src/nika_core/model_lab/__init__.py
@@ -0,0 +1,25 @@
+from nika_core.model_lab.contracts import (
+    BenchmarkReport,
+    CandidateBenchmarkSummary,
+    EvaluationCase,
+    EvaluationSplit,
+    EvaluationSuite,
+    ExactMatchScorer,
+    ModelBenchmarkPolicy,
+    ModelCandidate,
+    QualityScorer,
+)
+from nika_core.model_lab.service import ModelEngineeringLab
+
+__all__ = [
+    "BenchmarkReport",
+    "CandidateBenchmarkSummary",
+    "EvaluationCase",
+    "EvaluationSplit",
+    "EvaluationSuite",
+    "ExactMatchScorer",
+    "ModelBenchmarkPolicy",
+    "ModelCandidate",
+    "ModelEngineeringLab",
+    "QualityScorer",
+]
