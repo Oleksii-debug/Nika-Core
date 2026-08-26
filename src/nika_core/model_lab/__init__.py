250bc9fb1bcdf79487e15f7078179bb4303134a5
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-26 23:31:58 +0300

    F6: export Model Engineering Lab API

---
+++ src/nika_core/model_lab/__init__.py
@@ -0,0 +1,47 @@
+from nika_core.model_lab.benchmark import (
+    ExactMatchScorer,
+    ModelBenchmarkRunner,
+    evidence_document,
+    evidence_sha256,
+    metric_means,
+    suite_sha256,
+)
+from nika_core.model_lab.contracts import (
+    AttemptStatus,
+    BenchmarkAttempt,
+    BenchmarkCase,
+    BenchmarkRunEvidence,
+    BenchmarkScorer,
+    BenchmarkSuite,
+    MetricValue,
+    ModelCandidate,
+)
+from nika_core.model_lab.experiment_adapter import (
+    build_experiment_definition,
+    candidate_identity_sha256,
+    candidate_strategy_ref,
+    metric_observations,
+    suite_replays,
+)
+
+__all__ = [
+    "AttemptStatus",
+    "BenchmarkAttempt",
+    "BenchmarkCase",
+    "BenchmarkRunEvidence",
+    "BenchmarkScorer",
+    "BenchmarkSuite",
+    "ExactMatchScorer",
+    "MetricValue",
+    "ModelBenchmarkRunner",
+    "ModelCandidate",
+    "build_experiment_definition",
+    "candidate_identity_sha256",
+    "candidate_strategy_ref",
+    "evidence_document",
+    "evidence_sha256",
+    "metric_means",
+    "metric_observations",
+    "suite_replays",
+    "suite_sha256",
+]
