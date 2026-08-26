75cb37e3a305bb6d70809563895e2f3de4a698fd
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-08-26 23:44:45 +0300

    MODEL-LAB: expose public evaluation API

---
+++ src/nika_core/model_engineering/__init__.py
@@ -0,0 +1,21 @@
+from .contracts import (
+    CaseMeasurement,
+    EvaluationCase,
+    MetricDefinition,
+    MetricValue,
+    ModelCandidate,
+    ModelExperimentSpec,
+    ModelRecommendation,
+)
+from .lab import ModelEngineeringLab
+
+__all__ = [
+    "CaseMeasurement",
+    "EvaluationCase",
+    "MetricDefinition",
+    "MetricValue",
+    "ModelCandidate",
+    "ModelEngineeringLab",
+    "ModelExperimentSpec",
+    "ModelRecommendation",
+]
