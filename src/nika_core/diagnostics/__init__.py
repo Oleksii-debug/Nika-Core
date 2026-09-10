1e09fc7d55d69452dc8ef86ef22a8e0b655c853a
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-10 19:12:53 +0200

    Diagnostics: export local model health contract

--- src/nika_core/diagnostics/__init__.py
+++ src/nika_core/diagnostics/__init__.py
@@ -1,3 +1,20 @@
 from nika_core.diagnostics.health import HealthCheck, HealthReport, HealthService, HealthStatus
+from nika_core.diagnostics.model_health import (
+    ModelHealthFact,
+    ModelHealthProbePort,
+    ModelHealthSnapshot,
+    ModelInferenceEvidencePort,
+    OllamaModelHealthProbe,
+)
 
-__all__ = ["HealthCheck", "HealthReport", "HealthService", "HealthStatus"]
+__all__ = [
+    "HealthCheck",
+    "HealthReport",
+    "HealthService",
+    "HealthStatus",
+    "ModelHealthFact",
+    "ModelHealthProbePort",
+    "ModelHealthSnapshot",
+    "ModelInferenceEvidencePort",
+    "OllamaModelHealthProbe",
+]
