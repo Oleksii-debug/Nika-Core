45be91cd995c2d5d4999c7a79d4f6facb016462a
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-12 08:32:53 +0200

    style(research): fix canonical stdlib import order

--- src/nika_core/research/network_repository.py
+++ src/nika_core/research/network_repository.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from contextvars import ContextVar
-from datetime import UTC, datetime
 import hashlib
 import json
+from contextvars import ContextVar
+from datetime import UTC, datetime
 from uuid import uuid4
 
 from nika_core.data.sqlite import SQLiteStore
