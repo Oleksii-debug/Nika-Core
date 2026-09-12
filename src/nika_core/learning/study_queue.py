274abdd8c70d7a99b764915b20ac0c015f74ef66
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-12 14:50:16 +0200

    learning: apply exact Ruff import format

--- src/nika_core/learning/study_queue.py
+++ src/nika_core/learning/study_queue.py
@@ -8,9 +8,7 @@ import json
 import string
 import urllib.parse
 
-from nika_core.kernel import task_queue
-from nika_core.kernel import task_state
-
+from nika_core.kernel import task_queue, task_state
 
 _STUDY_PAYLOAD_KIND = "study_material_v1"
 _EVIDENCE_POLICY = "source_bound_v1"
