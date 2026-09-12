6429de9dd6a4dcf6971ab6ab0dc77edbd0c73401
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-12 19:57:48 +0200

    Add training artifact verification package

---
+++ src/nika_core/training_artifacts/__init__.py
@@ -0,0 +1,13 @@
+from nika_core.training_artifacts.integrity import (
+    CandidateArtifactIntegrityError,
+    ExpectedCandidateArtifact,
+    VerifiedCandidateArtifact,
+    verify_candidate_artifact,
+)
+
+__all__ = [
+    "CandidateArtifactIntegrityError",
+    "ExpectedCandidateArtifact",
+    "VerifiedCandidateArtifact",
+    "verify_candidate_artifact",
+]
